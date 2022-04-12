ed93bd590fed3518a19cb2950161d8662dda20c8
Manthan Kuber <manthankuber873@gmail.com>
2022-04-12 20:16:27 +0530

    Navigating Programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+
 
 const Home = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("Placing Order");
+    router.push('/product')
+  };
   return (
     <>
       <h1>Home Page</h1>
@@ -10,6 +17,7 @@ const Home = () => {
       <Link href="/product">
         <a>products</a>
       </Link>
+      <button onClick={handleClick}>Place Order</button>
     </>
   );
 };
