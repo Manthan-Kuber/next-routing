5008e65838c35656adf3543e6cd4958b7f0919a9
Manthan Kuber <manthankuber873@gmail.com>
2022-04-12 20:04:17 +0530

    Link Component Navigation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <Link href="/product">
+        <a>products</a>
+      </Link>
+    </>
+  );
 };
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,31 @@
-function Product() {
+import Link from "next/link";
+
+function Product({ productId = 100 }) {
   return (
     <>
-      <h1>Product 1</h1>
-      <h1>Product 2</h1>
-      <h1>Product 3</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/product/1">
+        <h1>
+          <a>Product 1</a>
+        </h1>
+      </Link>
+      <Link href="/product/2">
+        <h1>
+          <a>Product 2</a>
+        </h1>
+      </Link>
+      <Link href="/product/3" replace>
+        <h1>
+          <a>Product 3</a>
+        </h1>
+      </Link>
+      <Link href={`/product/${productId}`}>
+        <h1>
+          <a>Product {productId}</a>
+        </h1>
+      </Link>
     </>
   );
 }
